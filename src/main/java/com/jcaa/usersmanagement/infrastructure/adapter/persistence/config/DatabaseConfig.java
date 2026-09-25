f350a4123a94589f4fed24089eb729aff6515184
yefrey <yefreycassiani16@gmail.com>
2026-09-25 14:09:56 -0500

    Fix SSL mode for Render external DB connection

--- src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
@@ -3,7 +3,7 @@ package com.jcaa.usersmanagement.infrastructure.adapter.persistence.config;
 public record DatabaseConfig(
     String host, int port, String databaseName, String username, String password) {
   private static final String URL_TEMPLATE =
-      "jdbc:postgresql://%s:%d/%s?sslmode=prefer";
+      "jdbc:postgresql://%s:%d/%s?sslmode=require";
 
   public String buildJdbcUrl() {
     return String.format(URL_TEMPLATE, host, port, databaseName);
