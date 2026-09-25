839e20e4d90e904ab64a4c213095978b5cf1eb4b
yefrey <yefreycassiani16@gmail.com>
2026-09-25 14:25:41 -0500

    Revert SSL mode to prefer

--- src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/adapter/persistence/config/DatabaseConfig.java
@@ -3,7 +3,7 @@ package com.jcaa.usersmanagement.infrastructure.adapter.persistence.config;
 public record DatabaseConfig(
     String host, int port, String databaseName, String username, String password) {
   private static final String URL_TEMPLATE =
-      "jdbc:postgresql://%s:%d/%s?sslmode=require";
+      "jdbc:postgresql://%s:%d/%s?sslmode=prefer";
 
   public String buildJdbcUrl() {
     return String.format(URL_TEMPLATE, host, port, databaseName);
